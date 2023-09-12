2377283d0ceca9d1a571c60e09aa0fb4a2589d5a
Sebastian Wittrock <sebastianwittrock927@gmail.com>
2023-09-12 15:07:46 -0400

    implement multiply operation

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,7 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
+  public multiply(x: number, y: number) : number {
+    return x * y;
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-console.log(`result_add = ${result_add}`);
+const result_mul = calc.multiply(2, 3);
 
+console.log(`result_add = ${result_add}`);
+console.log(`result_mul = ${result_mul}`);
