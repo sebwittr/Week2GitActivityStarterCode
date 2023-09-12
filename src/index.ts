22f3ba2c1fbb010611ee73c5c18bf501effe2012
SpencerMullen <spencermullen0@gmail.com>
2023-09-12 15:07:34 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ const result_mul = calc.mul(2,3);
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
 console.log(`result_mul = ${result_mul}`);
+
+const result_divide = calc.divide(6, 3);
+console.log(`result_divide = ${result_divide}`);
