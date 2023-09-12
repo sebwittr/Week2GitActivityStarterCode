ce7f846db5dfc0c6f5d32225420ac111120550e2
SpencerMullen <spencermullen0@gmail.com>
2023-09-12 15:32:54 -0400

    divide

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const result_add = calc.add(2, 3);
 const result_sub = calc.subtract(4, 1);
 const result_mul = calc.mul(2,3);
 console.log(`result_add = ${result_add}`);
-console.log(`result_sub = ${result_sub}`);
+console.log(`result_sub = ${result_sub}`);  
 console.log(`result_mul = ${result_mul}`);
 
 const result_divide = calc.divide(6, 3);
