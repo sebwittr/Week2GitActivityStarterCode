1df5ae041c344d12e57e30a41414fefbe183328d
Sebastian Wittrock <sebastianwittrock927@gmail.com>
2023-09-12 14:59:03 -0400

    add subtract operation

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
+const result_sub = calc.subtract(4, 1);
 console.log(`result_add = ${result_add}`);
+console.log(`result_sub = ${result_sub}`);
 
